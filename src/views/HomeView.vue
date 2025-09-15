ba9c895ed2663c89f7e69c42f562d034c3b78cfc
Alex <133679349+alexmen656@users.noreply.github.com>
2025-09-15 11:20:16 +0200

    feat: Reorder and rename quick actions

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -99,18 +99,18 @@
                 <span>Log weight</span>
             </div>
             
-            <div class="action-chip" @click="goToView('water')">
-                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
-                    <path d="M12,2C13.73,7.21 16.69,9.88 16.69,13.58C16.69,17.65 14.54,19.12 12,19.12C9.46,19.12 7.31,17.65 7.31,13.58C7.31,9.88 10.27,7.21 12,2Z"/>
-                </svg>
-                <span>Water</span>
-            </div>
-            
             <div class="action-chip" @click="goToView('feedback')">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
                     <path d="M12,17.27L18.18,21L16.54,13.97L22,9.24L14.81,8.62L12,2L9.19,8.62L2,9.24L7.45,13.97L5.82,21L12,17.27Z"/>
                 </svg>
-                <span>Feedback</span>
+                <span>Give Feedback</span>
+            </div>
+            
+             <div class="action-chip" @click="goToView('water')">
+                <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12,2C13.73,7.21 16.69,9.88 16.69,13.58C16.69,17.65 14.54,19.12 12,19.12C9.46,19.12 7.31,17.65 7.31,13.58C7.31,9.88 10.27,7.21 12,2Z"/>
+                </svg>
+                <span>Water</span>
             </div>
             
             <div class="action-chip" @click="goToView('goals')">
